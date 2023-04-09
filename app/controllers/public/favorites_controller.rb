d49d3d3b666aeb8f1be04cf8b08cb6ac2df91292
kaitoppo <122006842+kaitoppo@users.noreply.github.com>
2023-04-09 21:56:13 +0000

    いいね非同期通信_修正

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -2,16 +2,16 @@ class Public::FavoritesController < ApplicationController
   before_action :authenticate_end_user!
 
   def create
-    post_machineko = PostMachineko.find(params[:post_machineko_id])
-    current_end_user.favorites.find_or_create_by(post_machineko_id: post_machineko.id)
-    redirect_to request.referer #同じページへ遷移
+    @post_machineko = PostMachineko.find(params[:post_machineko_id])
+    current_end_user.favorites.find_or_create_by(post_machineko_id: @post_machineko.id)
+    # redirect_to request.referer #同じページへ遷移
   end
 
   def destroy
-    post_machineko = PostMachineko.find(params[:post_machineko_id])
-    favorite = current_end_user.favorites.find_by(post_machineko_id: post_machineko.id)
+    @post_machineko = PostMachineko.find(params[:post_machineko_id])
+    favorite = current_end_user.favorites.find_by(post_machineko_id: @post_machineko.id)
     favorite.destroy if favorite
-    redirect_to request.referer #同じページへ遷移
+    # redirect_to request.referer #同じページへ遷移
   end
 
   def index
--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -2,16 +2,18 @@ class Public::PostCommentsController < ApplicationController
   before_action :authenticate_end_user!
 
   def create
-    post_machineko = PostMachineko.find(params[:post_machineko_id])
-    comment = current_end_user.post_comments.new(post_comment_params)
-    comment.post_machineko_id = post_machineko.id
-    comment.save
-    redirect_to post_machineko_path(post_machineko)
+    @post_machineko = PostMachineko.find(params[:post_machineko_id])
+    @post_comment = current_end_user.post_comments.new(post_comment_params)
+    @post_comment.post_machineko_id = @post_machineko.id
+    @post_comment.save
+    # redirect_to post_machineko_path(post_machineko)
   end
 
   def destroy
     PostComment.find(params[:id]).destroy
-    redirect_to request.referer
+    @post_machineko = PostMachineko.find(params[:post_machineko_id])
+    @post_comment = PostComment.new
+    # redirect_to request.referer
   end
 
 private
