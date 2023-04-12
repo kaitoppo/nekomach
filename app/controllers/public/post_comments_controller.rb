2be2ccd6af4f9b840ad1acf9ad58ca3ce3eb8670
kaitoppo <122006842+kaitoppo@users.noreply.github.com>
2023-04-12 03:08:24 +0000

    非同期通信_更新

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -3,16 +3,17 @@ class Public::PostCommentsController < ApplicationController
 
   def create
     @post_machineko = PostMachineko.find(params[:post_machineko_id])
-    @post_comment = current_end_user.post_comments.new(post_comment_params)
-    @post_comment.post_machineko_id = @post_machineko.id
-    @post_comment.save
+    @post_comment = PostComment.new
+    comment = current_end_user.post_comments.new(post_comment_params)
+    comment.post_machineko_id = @post_machineko.id
+    comment.save
     # redirect_to post_machineko_path(post_machineko)
   end
 
   def destroy
-    PostComment.find(params[:id]).destroy
     @post_machineko = PostMachineko.find(params[:post_machineko_id])
-    @post_comment = PostComment.new
+    post_comment = @post_machineko.post_comments.find(params[:id])
+    post_comment.destroy
     # redirect_to request.referer
   end
 
